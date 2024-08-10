7711d5abb9225786f183d7cd2fd2e05f7730f8c1
rhuanbarros@gmail.com <rhuanbarros@gmail.com>
2024-08-10 16:32:29 +0000

    Created AboutUs, LayoutPage and GenerateQuestions; Edited settings; Deleted Form1

---
+++ client_code/LayoutPage/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import LayoutPageTemplate
+from anvil import *
+
+
+class LayoutPage(LayoutPageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def link_about_us_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('AboutUs')
+
+  def link_generate_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('GenerateQuestions')
+
+  def reset_links(self, **event_args):
+    self.link_about_us.role = ''
