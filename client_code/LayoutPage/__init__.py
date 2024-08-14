22b9abd2bc2369ec76be918811e63580f2341caf
rhuanbarros@gmail.com <rhuanbarros@gmail.com>
2024-08-14 23:45:10 +0000

    Edited ServerModule_show_question, ShowQuestion and LayoutPage

--- client_code/LayoutPage/__init__.py
+++ client_code/LayoutPage/__init__.py
@@ -8,28 +8,27 @@ from anvil.tables import app_tables
 
 
 class LayoutPage(LayoutPageTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    anvil.users.login_with_form()
-    
+        anvil.users.login_with_form()
 
-  def link_about_us_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('AboutUs')
+    def link_about_us_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        open_form("AboutUs")
 
-  def link_generate_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('GenerateQuestions')
+    def link_generate_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        open_form("GenerateQuestions")
 
-  def reset_links(self, **event_args):
-    self.link_about_us.role = ''
+    def reset_links(self, **event_args):
+        self.link_about_us.role = ""
 
-  def form_show(self, **event_args):
-    """This method is called when the form is shown on the page"""
-    pass
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
+        pass
 
-  def link_session_settings_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('SessionSettings')
+    def link_session_settings_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        open_form("SessionSettings")
--- client_code/ShowQuestion/__init__.py
+++ client_code/ShowQuestion/__init__.py
@@ -6,51 +6,61 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+from datetime import datetime
+import uuid
+
 
 class ShowQuestion(ShowQuestionTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+        # Any code you write here will run before the form opens.
 
-  def form_show(self, **event_args):
-    """This method is called when the form is shown on the page"""
-    self.get_question()
-    
-  def get_question(self):
-    self.column_panel_btnsanswer.visible = True
-    self.linear_panel_explanation.visible = False
-    
-    self.question = anvil.server.call('get_question')
-    self.label_question.text = self.question['question']
-    self.label_subject_matter.text = self.question['title']
-    self.label_level.text = self.question['level']
+    def form_show(self, **event_args):
+        """This method is called when the form is shown on the page"""
+        self.get_question()
 
-    
+    def get_question(self):
+        self.column_panel_btnsanswer.visible = True
+        self.linear_panel_explanation.visible = False
 
-  def verify_answer(self, answer: bool):
-    if answer == bool(self.question['answer_correct']):
-      alert("You got it right!")
-    else:
-      alert("You got it wrong!")
+        # self.question = anvil.server.call('get_question')
+        self.questions, self.answers = anvil.server.call("get_session")
 
-    anvil.server.call('save_answer', answer, self.question )
+        self.question = self.questions.search()[0]
 
-    self.column_panel_btnsanswer.visible = False
-    self.linear_panel_explanation.visible = True
+        self.label_question.text = self.question["question"]
+        self.label_subject_matter.text = self.question["title"]
+        self.label_level.text = self.question["level"]
 
-    self.label_explanation.text = self.question['explanation']
-    
+    def verify_answer(self, answer: bool):
+        if answer == bool(self.question["answer_correct"]):
+            alert("You got it right!")
+        else:
+            alert("You got it wrong!")
 
-  def button_true_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.verify_answer(True)
+        # anvil.server.call("save_answer", answer, self.question)
+        self.answers.add_row(
+            created_at=datetime.now(),
+            question=self.question,
+            got_it_right=answer == bool(self.question["answer_correct"]),
+            session=str(uuid.uuid4()),
+        )
 
-  def button_false_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.verify_answer(False)
+        self.column_panel_btnsanswer.visible = False
+        self.linear_panel_explanation.visible = True
 
-  def button_next_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.get_question()
+        self.label_explanation.text = self.question["explanation"]
+
+    def button_true_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.verify_answer(True)
+
+    def button_false_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.verify_answer(False)
+
+    def button_next_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.get_question()
