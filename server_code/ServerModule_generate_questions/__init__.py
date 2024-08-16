0a5d53bdad801878afd23a62346ac7b86e0ebb04
rhuanbarros@gmail.com <rhuanbarros@gmail.com>
2024-08-16 11:59:23 +0000

    generator gemini

--- server_code/ServerModule_generate_questions/__init__.py
+++ server_code/ServerModule_generate_questions/__init__.py
@@ -8,27 +8,30 @@ import anvil.server
 from datetime import datetime
 
 from . import generator_openai
+from . import generator_google_gemini
 
 
 @anvil.server.callable
 def generate_questions(title, parameters):
     print("------------------- generate_questions FUNCTION -------------------")
 
-    questionList = generator_openai.chain.invoke(parameters)
-    # questions = json_parser(response)
+    generator_google_gemini.generate_questions()
 
-    print(questionList.question_itens)
+    # questionList = generator_openai.chain.invoke(parameters)
+    # # questions = json_parser(response)
 
-    for question in questionList.question_itens:
-        # add to the database
-        app_tables.questions.add_row(
-            created_at=datetime.now(),
-            title=title,
-            topic_description=question.topic_description,
-            level=question.level,
-            question=question.question,
-            type="true_or_false",
-            answer_correct=question.answer_correct,
-            answers=None,
-            explanation=question.explanation,
-        )
+    # print(questionList.question_itens)
+
+    # for question in questionList.question_itens:
+    #     # add to the database
+    #     app_tables.questions.add_row(
+    #         created_at=datetime.now(),
+    #         title=title,
+    #         topic_description=question.topic_description,
+    #         level=question.level,
+    #         question=question.question,
+    #         type="true_or_false",
+    #         answer_correct=question.answer_correct,
+    #         answers=None,
+    #         explanation=question.explanation,
+    #     )
--- server_code/ServerModule_generate_questions/generator_google_gemini.py
+++ server_code/ServerModule_generate_questions/generator_google_gemini.py
@@ -5,15 +5,31 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+
+import os
+import google.generativeai as genai
+
+key_gemini = anvil.secrets.get_secret("GOOGLE_API_KEY")
+
+genai.configure(api_key=key_gemini)
+
+# Create the model
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "application/json",
+}
+
+llm_gemini = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+    # safety_settings = Adjust safety settings
+    # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+
+def generate_questions():
+    response = llm_gemini.generate_content("Write a story about an AI and magic")
+    print(response.text)
